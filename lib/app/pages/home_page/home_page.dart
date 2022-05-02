d4dd5dd8f0ee9ad0c05e4c11b9e2a4241e1cf613
Kauanphbbb <kauanphbbb@gmail.com>
2022-05-02 19:42:19 -0300

    feat: initial home page

--- lib/app/pages/home_page/home_page.dart
+++ lib/app/pages/home_page/home_page.dart
@@ -9,8 +9,33 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Meu pequeno negócio'),
       ),
-      body: const Center(
-        child: Text('Meu pequeno negócio'),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          height: MediaQuery.of(context).size.height * 0.5,
+          decoration: const BoxDecoration(
+            color: Colors.deepPurple,
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
+
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  Text('Entradas'),
+                  Text('Saídas')
+                ],
+              ),
+              Column(
+                children: const [
+                  Text('Balanço'),
+                  Text('R\$ 1000.00')
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
